416d712513f35173f8edf3b20a8542f39360e0fd
Priyanka <priyanka.giri@specter-automation.com>
2025-01-31 17:36:20 +0100

    responsive design

--- my-portfolio/src/app/components/header/header.component.ts
+++ my-portfolio/src/app/components/header/header.component.ts
@@ -1,17 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-  constructor(private router: Router) { }
+  menuOpen: boolean = false;
 
+  constructor(private router: Router) {}
+
+  // Function to handle navigation
   navigateTo(route: string) {
-    this.router.navigate([route]);
+    this.router.navigate([`/${route}`]);
+    this.menuOpen = false; // Close the menu after clicking (for mobile)
+  }
+
+  // Function to toggle mobile menu
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
   }
 }
